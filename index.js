b8d750d8868539f58644394c1c5ac4d87bffb13a
Bjoern Acker <bjoern@electronauts.net>
2017-12-21 09:08:39 +0100

    no message

--- index.js
+++ index.js
@@ -1174,12 +1174,13 @@ Jimp.prototype.autocrop = function() {
         );
     }
 
-    if (isNodePattern(cb)) return cb.call(this, null, {
+    if (isNodePattern(cb)) cb.call(this, null, {
         x: eastPixelsToCrop,
         y: northPixelsToCrop,
         width: widthOfPixelsToCrop,
         height: heightOfPixelsToCrop
     });
+
     return this;
 };
 
